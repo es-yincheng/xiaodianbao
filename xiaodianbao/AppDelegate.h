1e869686935a6f478f57b5396c7bb7fa601914a6
es-yincheng <yc@yinchengdeimac.local>
2016-06-29 17:08:17 +0800

    Initial Commit

---
+++ xiaodianbao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xiaodianbao
+//
+//  Created by 尹成 on 16/6/29.
+//  Copyright © 2016年 尹成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
